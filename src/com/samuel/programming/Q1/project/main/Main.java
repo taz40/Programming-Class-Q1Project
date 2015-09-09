b44ef1a33c4794f5428e225c22813a84300981f5
108147 <108147@SOUA221-5VCQJM1.lcsd1.laramie1.k12.wy.us>
2015-09-09 09:38:18 -0600

    Created main display.

--- src/com/samuel/programming/Q1/project/main/Main.java
+++ src/com/samuel/programming/Q1/project/main/Main.java
@@ -1,16 +1,32 @@
 package com.samuel.programming.Q1.project.main;
 
+import io.brace.lightsoutgaming.engine.LightsOut;
+
 import com.samuel.programming.Q1.project.references.Reference;
 
-public class Main {
+public class Main extends LightsOut {
+	
+	float aspectRatio = 9f/16f;
+	int width = 900;
+	int height = (int) (width * aspectRatio);
 	
 	public static void main(String[] args){
-		new Main();
+		new Main().init();
 	}
-	
-	public Main(){
-		System.out.println("The name of this project is " + Reference.projectName);
-		System.out.println("Version: " + Reference.version);
+
+	protected void init() {
+		createDisplay(Reference.projectName + " v. " + Reference.version, width, height);
+		start();
+	}
+
+	protected void render() {
+		screen.clear();
+		
+		show();
+	}
+
+	protected void update() {
+		
 	}
 
 }
