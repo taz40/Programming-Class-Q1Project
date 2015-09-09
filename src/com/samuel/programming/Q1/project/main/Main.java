c332fab9ceda09385c520f69f11f9e8273659cfa
108147 <108147@SOUA221-5VCQJM1.lcsd1.laramie1.k12.wy.us>
2015-09-09 09:24:58 -0600

    Created base classes.

---
+++ src/com/samuel/programming/Q1/project/main/Main.java
@@ -0,0 +1,15 @@
+package com.samuel.programming.Q1.project.main;
+
+import com.samuel.programming.Q1.project.references.Reference;
+
+public class Main {
+	
+	public static void main(String[] args){
+		new Main();
+	}
+	
+	public Main(){
+		System.out.println("The name of this project is " + Reference.projectName);
+	}
+
+}
---
+++ src/com/samuel/programming/Q1/project/references/Reference.java
@@ -0,0 +1,7 @@
+package com.samuel.programming.Q1.project.references;
+
+public class Reference {
+	
+	public static String projectName = "Q1 Project";
+
+}
